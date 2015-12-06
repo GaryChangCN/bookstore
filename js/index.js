053a9f24d26ce247ff76a6527d1b533e7061bb29
lgshuo <tinytin@qq.com>
2015-12-07 01:04:24 +0800

    12-07：1
    主页完工70%

--- js/index.js
+++ js/index.js
@@ -1,7 +1,32 @@
+jQuery(function($){
+    $('#slidebox').slideBox();
+    $('#slidebox').slideBox({
+        direction : 'left',//left,top#方向
+        duration : 0.8,//滚动持续时间，单位：秒
+        easing : 'swing',//swing,linear//滚动特效
+        delay : 5,//滚动延迟时间，单位：秒
+        startIndex : 1//初始焦点顺序
+    });
+});
 //DOM页面加载完成函数
 $(document).ready(function(){
-	$(".hearder-logo").css('margin-left',$(".content").css('margin-left'));
-	$("#btn").click(function(){
-		alert("ppp");
+	$(window).on("scroll",function(){
+		if($(window).scrollTop() > 60){
+			$(".hearder").stop().animate({
+				height:'40px'
+			}).css("position","fixed");
+			$(".hearder-logo").stop().animate({
+				width:'100px',
+				top:"8px"
+			},"fast")
+		}else{
+			$(".hearder").stop().animate({
+				height:'80px'
+			}).css('position','static');
+			$(".hearder-logo").stop().animate({
+				width:'200px',
+				top:"15px"
+			},"fast")
+		}
 	})
 })
