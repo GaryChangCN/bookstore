598f74197a96bcb29ea833316a04e98d8e498274
lgshuo <tinytin@qq.com>
2015-12-07 15:19:01 +0800

    12-07：2
    首页页面做完

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ jQuery(function($){
 });
 //DOM页面加载完成函数
 $(document).ready(function(){
+//	导航栏随着滚动放大缩小
 	$(window).on("scroll",function(){
 		if($(window).scrollTop() > 60){
 			$(".hearder").stop().animate({
@@ -18,7 +19,10 @@ $(document).ready(function(){
 			$(".hearder-logo").stop().animate({
 				width:'100px',
 				top:"8px"
-			},"fast")
+			},"fast");
+			$(".hearder-content").stop().animate({
+				height:'40px'
+			},"fast");
 		}else{
 			$(".hearder").stop().animate({
 				height:'80px'
@@ -26,7 +30,16 @@ $(document).ready(function(){
 			$(".hearder-logo").stop().animate({
 				width:'200px',
 				top:"15px"
-			},"fast")
+			},"fast");
+			$(".hearder-content").stop().animate({
+				height:'80px'
+			},"fast");
 		}
-	})
+	});
+//	导航栏鼠标悬浮变色
+    $(".HearderContentTable tr td").mouseover(function(){
+    	$(this).css('background-color',"#05A2EF");
+    }).mouseleave(function(){
+    	$(this).css('background-color','#54BAEC');
+    })
 })
