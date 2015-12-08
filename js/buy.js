e6398adf49c0f3226f1a08b849030d201c354a6d
lgshuo <tinytin@qq.com>
2015-12-08 23:28:42 +0800

    12-08
    买书页面解决

--- js/buy.js
+++ js/buy.js
@@ -9,6 +9,7 @@ jQuery(function($){
     });
 });
 $(document).ready(function(){
+	//slide买家须知折叠
 	$("#BuyersNoticeSlide").click(function(){
 		$("#BuyersNoticeContent").slideToggle();
 		$("#BuyersNoticeSlide").addClass("BuyersNoticeSlideDown");
@@ -16,5 +17,50 @@ $(document).ready(function(){
 	$(".BuyersNoticeSlideDown").click(function(){
 		$("#BuyersNoticeContent").slideToggle();
 		$("#BuyersNoticeSlideDown").removeClass("BuyersNoticeSlideDown");
+	});
+	//学期专业处理效果
+	  //点击展开分类
+	$(".SelectMajorContent ul ul").slideUp("fast");  
+	$(".SelectMajorContentFirst").on("click",function(){
+		if ($(this).next().is("ul")) {
+			$(".SMCFirst").slideUp();
+			if ($(this).next().css('display')=="block") {
+				$(this).next().slideUp();
+			} else{
+				$(this).next().slideDown();
+			}
+			$(this).parent().children("li").css({
+				"font-weight":"300",
+				"color":"#000000"
+			});
+			$(this).css({
+				"font-weight":"800",
+				"color":"#e05c49"
+			});
+		}
+	})
+	$(".SelectMajorContentSecond").on("click",function(){
+		if ($(this).next().is("ul")) {
+			$(".SMCSecond").slideUp();
+			if ($(this).next().css('display')=="block") {
+				$(this).next().slideUp();
+			} else{
+				$(this).next().slideDown();
+			}
+			$(".SelectMajorContentSecond").css({
+				"font-weight":"100",
+				"color":"#000000"
+			});
+			$(this).css({
+				"font-weight":"600",
+				"color":"#e05c49"
+			});
+		}
+	});
+	//===============================================================
+	//---------------------------------------------------------------
+	$(".PageNumber ul li:nth-child(4)").css({
+		"background-color":"#e07262",
+		"color":"white"
 	})
 })
