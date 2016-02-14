9636670deb88ceb7c774eb65e8a9f069e2b81909
lgshuo <tinytin@qq.com>
2016-02-14 19:38:38 +0800

    2016-02-14

--- admin/visited.php
+++ admin/visited.php
@@ -8,7 +8,8 @@ if ($certain == 'admin' || $certain == 'server') {
 			$count = mysql_query("SELECT COUNT(*) FROM b_visited");
 			$rowcount = mysql_fetch_array($count);
 			echo $rowcount[0] . "#";
-			$query = mysql_query("SELECT * FROM b_visited");
+			$num=($_POST['num']-1)*10;
+			$query = mysql_query("SELECT * FROM b_visited LIMIT $num,10");
 			while ($row = mysql_fetch_array($query)) {
 				echo "<tr>" . "<td>" . $row[1] . "-" . $row[2] . "-" . $row[3] . "</td><td>" . $row[4] . "</td><td>" . $row[5] . "</td>" . "</tr>";
 			}
@@ -18,7 +19,8 @@ if ($certain == 'admin' || $certain == 'server') {
 			$count = mysql_query("SELECT COUNT(*) FROM b_visited WHERE year='$year'");
 			$rowcount = mysql_fetch_array($count);
 			echo $rowcount[0] . "#";
-			$query = mysql_query("SELECT * FROM b_visited WHERE year='$year'");
+			$num=($_POST['num']-1)*10;
+			$query = mysql_query("SELECT * FROM b_visited WHERE year='$year' LIMIT $num,10");
 			while ($row = mysql_fetch_array($query)) {
 				echo "<tr>" . "<td>" . $row[1] . "-" . $row[2] . "-" . $row[3] . "</td><td>" . $row[4] . "</td><td>" . $row[5] . "</td>" . "</tr>";
 			}
@@ -28,7 +30,8 @@ if ($certain == 'admin' || $certain == 'server') {
 			$count = mysql_query("SELECT COUNT(*) FROM b_visited WHERE mouth='$mouth'");
 			$rowcount = mysql_fetch_array($count);
 			echo $rowcount[0] . "#";
-			$query = mysql_query("SELECT * FROM b_visited WHERE mouth='$mouth'");
+			$num=($_POST['num']-1)*10;
+			$query = mysql_query("SELECT * FROM b_visited WHERE mouth='$mouth' LIMIT $num,10");
 			while ($row = mysql_fetch_array($query)) {
 				echo "<tr>" . "<td>" . $row[1] . "-" . $row[2] . "-" . $row[3] . "</td><td>" . $row[4] . "</td><td>" . $row[5] . "</td>" . "</tr>";
 			}
@@ -45,6 +48,51 @@ if ($certain == 'admin' || $certain == 'server') {
 			$query = mysql_query("SELECT * FROM b_visited WHERE year='$year' AND mouth='$mouth' AND day='$day'");
 			while ($row = mysql_fetch_array($query)) {
 				echo "<tr>" . "<td>" . $row[1] . "-" . $row[2] . "-" . $row[3] . "</td><td>" . $row[4] . "</td><td>" . $row[5] . "</td>" . "</tr>";
+			}
+			break;
+		case '5' :
+			switch ($_POST['type2']) {//1全部 2按年 3 按月 4按天
+				case '1' :
+					$a = mysql_query("SELECT COUNT(*) FROM b_visited");
+					$b = mysql_fetch_array($a);
+					$num1 = ceil($b[0] / 10);
+					for ($i = 1; $i <= $num1; $i++) {
+						echo "<li>" . ($num1 + 1 - $i) . "</li>";
+					}
+					break;
+				case '2' :
+					$time = $_POST['data'];
+					$a = mysql_query("SELECT COUNT(*) FROM b_visited WHERE year=$time");
+					$b = mysql_fetch_array($a);
+					$num1 = ceil($b[0] / 10);
+					for ($i = 1; $i <= $num1; $i++) {
+						echo "<li>" . ($num1 + 1 - $i) . "</li>";
+					}
+					break;
+				case '3' :
+					$time = $_POST['data'];
+					$a = mysql_query("SELECT COUNT(*) FROM b_visited WHERE mouth=$time");
+					$b = mysql_fetch_array($a);
+					$num1 = ceil($b[0] / 10);
+					for ($i = 1; $i <= $num1; $i++) {
+						echo "<li>" . ($num1 + 1 - $i) . "</li>";
+					}
+					break;
+				case '4' :
+					$time = $_POST['data'];
+					$d = explode("-", $time);
+					$y = $d[0];
+					$m = $d[1];
+					$da = $d[2];
+					$a = mysql_query("SELECT COUNT(*) FROM b_visited WHERE year='$year' AND mouth='$mouth' AND day='$day'");
+					$b = mysql_fetch_array($a);
+					$num1 = ceil($b[0] / 10);
+					for ($i = 1; $i <= $num1; $i++) {
+						echo "<li>" . ($num1 + 1 - $i) . "</li>";
+					}
+					break;
+				default :
+					break;
 			}
 			break;
 		default :
