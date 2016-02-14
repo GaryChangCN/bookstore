0ca903762fda4839a16673085bfae9fedb6595c4
lgshuo <tinytin@qq.com>
2016-02-14 19:38:28 +0800

    2016-02-14

--- admin/Our.php
+++ admin/Our.php
@@ -5,7 +5,8 @@ include "conn.php";
 $type = $_POST['type'];
 switch ($type) {
 	case '1' :
-		$query1 = mysql_query("SELECT * FROM b_our");
+		$limit=($_POST['num']-1)*5;
+		$query1 = mysql_query("SELECT * FROM b_our LIMIT $limit,5");
 		while ($row1 = mysql_fetch_array($query1)) {
 			if ($row1['display'] == "1") {
 				echo "<li class='selected'>编号：" . $row1['id'] . "-<br/>" . $row1['text'] . "</li>";
@@ -31,6 +32,14 @@ switch ($type) {
 		mysql_query("INSERT INTO b_our VALUES('','$text','0')");
 		echo "添加成功";
 		break;
+	case '5':
+		$a = mysql_query("SELECT COUNT(*) FROM b_our");
+			$b = mysql_fetch_array($a);
+			$num1 = ceil($b[0] / 5);
+			for ($i = 1; $i <= $num1; $i++) {
+				echo "<li>" . ($num1 + 1 - $i) . "</li>";
+			}
+		break;	
 	default :
 		break;
 }
--- admin/js/BestSell.js
+++ admin/js/BestSell.js
@@ -1,24 +1,86 @@
 $(document).ready(function() {
+	//type 1 获取页码 type  2  切换页码----->SelectGood.php
 	var second = "0";
-	$.ajax({
+	$.ajax({ //获取总页码
 		type: "post",
 		url: "../SelectGood.php",
-		async: true,
-		success: function(data) {
-			$("#ulLeft").html(data);
-		}
-	});
-	$.ajax({
-		type:"post",
-		url:"../BestSell.php",
-		async:false,
-		data:{
-			"type":"2"
+		async: false,
+		data: {
+			"type": "1"
 		},
-		success:function(data){
-			$("#ulRight").html("<li>id</li>"+data);
+		success: function(data) {
+			//$(".PageNumber").html(data);
 		}
 	});
+	changePageNumber("1");
+    pageNum("1");
+	function changePageNumber() {
+		$.ajax({
+			type: "post",
+			url: "../SelectGood.php",
+			async: false,
+			data: {
+				"type": "2",
+				"num": arguments[0]
+			},
+			success: function(d) {
+				$("#ulLeft").html(d);
+			}
+		});
+	}
+	$(".PageNumber").on("click", "li", function() { //改变页码
+			$(this).parent().children().css({
+				'background-color': "transparent",
+				'color': '#0392DC'
+			});
+			$(this).css({
+				'background-color': "#0392DC",
+				'color': 'white'
+			})
+			var num = $(this).text();
+			changePageNumber(num);
+		})
+		//BestSell type----1添加热门商品  2获取热门商品  3 删除  type2----1获取列表  2选择页面
+	$.ajax({
+		type: "post",
+		url: "../BestSell.php",
+		async: false,
+		data: {
+			"type": "2",
+			"type2": "1"
+		},
+		success: function(data) {
+			$(".PageNumber2").html(data);
+		}
+	});
+    $(".PageNumber2").on("click", "li", function() { //改变页码
+			$(this).parent().children().css({
+				'background-color': "transparent",
+				'color': '#0392DC'
+			});
+			$(this).css({
+				'background-color': "#0392DC",
+				'color': 'white'
+			})
+			var num = $(this).text();
+			pageNum(num);
+		})
+	function pageNum() {
+		$.ajax({
+			type: "post",
+			url: "../BestSell.php",
+			async: false,
+			data: {
+				"type": "2",
+				"type2":"2",
+				"num":arguments[0]
+			},
+			success: function(data) {
+				$("#ulRight").html("<li>id</li>" + data);
+			}
+		});
+	}
+
 	$("#ulLeft").on("click", "li", function() {
 		if ($(this).css('background-color') == "rgb(211, 211, 211)") {
 			$(this).css('background-color', "transparent");
@@ -26,21 +88,21 @@ $(document).ready(function() {
 			$(this).css("background-color", "#D3D3D3");
 		}
 	});
-	$("#ulRight").on("dblclick","li",function(){
-		var r=confirm("确定要移除热销商品"+$(this).text().split("#")[0]+"吗？");
-		if(r==true){
+	$("#ulRight").on("dblclick", "li", function() {
+		var r = confirm("确定要移除热销商品" + $(this).text().split("#")[0] + "吗？");
+		if (r == true) {
 			$.ajax({
-				type:"post",
-				url:"../BestSell.php",
-				async:false,
-				data:{
-					"type":"3",
-					"name":$(this).text().split("#")[0],
-					"isbn":$(this).text().split("#")[1]
+				type: "post",
+				url: "../BestSell.php",
+				async: false,
+				data: {
+					"type": "3",
+					"name": $(this).text().split("#")[0],
+					"isbn": $(this).text().split("#")[1]
 				},
-				success:function(data){
+				success: function(data) {
 					alert(data);
-					window.location.href="";
+					window.location.href = "";
 				}
 			});
 		}
@@ -56,18 +118,18 @@ $(document).ready(function() {
 		});
 		if (second == "0") {
 			alert("你还没有选择要添加 的内容!");
-		} else{
+		} else {
 			$.ajax({
-				type:"post",
-				url:"../BestSell.php",
-				async:false,
-				data:{
-					"type":"1",
-					"name":second
+				type: "post",
+				url: "../BestSell.php",
+				async: false,
+				data: {
+					"type": "1",
+					"name": second
 				},
-				success:function(data){
+				success: function(data) {
 					alert(data);
-					window.location.href="";
+					window.location.href = "";
 				}
 			});
 		}
