574f1e47f6cc3ad2b09ba132ffc9cac4ab887397
lgshuo <tinytin@qq.com>
2016-02-24 12:08:25 +0800

    bug修复

--- js/input.js
+++ js/input.js
@@ -1,89 +1,109 @@
-$("#phonenumber").on('focus', function() {
-	PhoneNumberCorrect()
-});
-$("#xiaoqu").on('focus', function() {
-	XiaoQuCorrect();
-	if ($("#phonenumber").val() == "") {
-		PhoneNumberError()
-	}
-});
-$("#loudao").on('focus',function(){
-	LouDaoCorrect();
-	if($("#xiaoqu").val()==''){
-		XiaoQuError();
-	}
-});
-$("#qinshi").on('focus',function(){
-	QinShiCorrect()
-	if($("#loudao").val()==""){
-		LouDaoError();
+$(document).ready(function() {
+	$("#phonenumber").on('focus', function() {
+		PhoneNumberCorrect();
+		checksubmit();
+	});
+	$("#xiaoqu").on('focus', function() {
+		XiaoQuCorrect();
+		if ($("#phonenumber").val() == "") {
+			PhoneNumberError()
+		}
+		checksubmit();
+	});
+	$("#loudao").on('focus', function() {
+		LouDaoCorrect();
+		if ($("#xiaoqu").val() == '') {
+			XiaoQuError();
+		}
+		checksubmit();
+	});
+	$("#qinshi").on('focus', function() {
+		QinShiCorrect()
+		if ($("#loudao").val() == "") {
+			LouDaoError();
+		};
+		checksubmit();
+	});
+	$("#remark").on('focus', function() {
+		if ($("#qinshi").val() == "") {
+			QinShiError()
+		}
+	});
+//	$("#InputSubmit input:last-child").click(function() {
+//		if (($("#phonenumber").val() != "") && ($("#xiaoqu").val() != "") && ($("#loudao").val() != "") && ($("#qinshi").val() != "")) {
+//			alert("提交");
+//		} else {
+//			alert("对不起你信息没有填写完整");
+//		}
+//	})
+    function checksubmit(){
+    	if (($("#phonenumber").val() != "") && ($("#xiaoqu").val() != "") && ($("#loudao").val() != "") && ($("#qinshi").val() != "")) {
+			$("#enterbuy").removeAttr('disabled');
+		}else{
+			$("#enterbuy").attr('disabled','disabled');
+		}
+    }
+	function PhoneNumberError() {
+		$("#phonenumber").css({
+			'borderStyle': 'solid',
+			'boederWidth': '5px',
+			'borderColor': "red"
+		}).attr('placeholder', '你还没有输入手机号！！！')
 	};
-});
-$("#remark").on('focus',function(){
-	if($("#qinshi").val()==""){
-		QinShiError()
-	}
-});
-$("#InputSubmit input:last-child").click(function(){
-	if(($("#phonenumber").val()!="")&&($("#xiaoqu").val()!="")&&($("#loudao").val()!="")&&($("#qinshi").val()!="")){
-	    alert("提交");
-	}else{
-		alert("对不起你信息没有填写完整");
-	}
+
+	function PhoneNumberCorrect() {
+		$("#phonenumber").css({
+			'borderStyle': 'dashed',
+			'boederWidth': '1px',
+			'borderColor': "#e05c49"
+		}).attr('placeholder', "唯一联系方式请仔细填写");
+	};
+
+	function XiaoQuError() {
+		$("#xiaoqu").css({
+			'borderStyle': 'solid',
+			'boederWidth': '5px',
+			'borderColor': "red"
+		}).attr('placeholder', '你还没有输入小区号');
+	};
+
+	function XiaoQuCorrect() {
+		$("#xiaoqu").css({
+			'borderStyle': 'dashed',
+			'boederWidth': '1px',
+			'borderColor': "#e05c49"
+		}).attr('placeholder', "例：9小区");
+	};
+
+	function LouDaoError() {
+		$("#loudao").css({
+			'borderStyle': 'solid',
+			'boederWidth': '5px',
+			'borderColor': "red"
+		}).attr('placeholder', '你还没有输入楼道号');
+	};
+
+	function LouDaoCorrect() {
+		$("#loudao").css({
+			'borderStyle': 'dashed',
+			'boederWidth': '1px',
+			'borderColor': "#e05c49"
+		}).attr('placeholder', "例：a073");
+	};
+
+	function QinShiError() {
+		$("#qinshi").css({
+			'borderStyle': 'solid',
+			'boederWidth': '5px',
+			'borderColor': "red"
+		}).attr('placeholder', '你还没有输入寝室号');
+	};
+
+	function QinShiCorrect() {
+		$("#qinshi").css({
+			'borderStyle': 'dashed',
+			'boederWidth': '1px',
+			'borderColor': "#e05c49"
+		}).attr('placeholder', "例：301");
+	};
 })
-function PhoneNumberError(){
-	$("#phonenumber").css({
-			'borderStyle': 'solid',
-			'boederWidth': '5px',
-			'borderColor': "red"
-		}).attr('placeholder','你还没有输入手机号！！！')
-};
-function PhoneNumberCorrect(){
-	$("#phonenumber").css({
-		'borderStyle': 'dashed',
-		'boederWidth': '1px',
-		'borderColor': "#e05c49"
-	}).attr('placeholder',"唯一联系方式请仔细填写");
-};
-function XiaoQuError(){
-	$("#xiaoqu").css({
-			'borderStyle': 'solid',
-			'boederWidth': '5px',
-			'borderColor': "red"
-		}).attr('placeholder','你还没有输入小区号');
-};
-function XiaoQuCorrect(){
-	$("#xiaoqu").css({
-		'borderStyle': 'dashed',
-		'boederWidth': '1px',
-		'borderColor': "#e05c49"
-	}).attr('placeholder',"例：9小区");
-};
-function LouDaoError(){
-	$("#loudao").css({
-			'borderStyle': 'solid',
-			'boederWidth': '5px',
-			'borderColor': "red"
-		}).attr('placeholder','你还没有输入楼道号');
-};
-function LouDaoCorrect(){
-	$("#loudao").css({
-		'borderStyle': 'dashed',
-		'boederWidth': '1px',
-		'borderColor': "#e05c49"
-	}).attr('placeholder',"例：a073");
-};
-function QinShiError(){
-	$("#qinshi").css({
-			'borderStyle': 'solid',
-			'boederWidth': '5px',
-			'borderColor': "red"
-		}).attr('placeholder','你还没有输入寝室号');
-};
-function QinShiCorrect(){
-	$("#qinshi").css({
-		'borderStyle': 'dashed',
-		'boederWidth': '1px',
-		'borderColor': "#e05c49"
-	}).attr('placeholder',"例：301");
-};
