5a34d04b45e3794a9d44469c158ca47800df3a23
张贯宇 <garychangcn@hotmail.com>
2016-05-13 22:47:21 +0800

    admin

---
+++ a/js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$(".left,.right").css('height',$(window).height()-62+"px");
+	$("iframe").css('height',$(window).height()-64+"px");
+	$(".left li:first-child").css('backgroundColor','#eee');
+	$(".left li").on('mouseover', function(event) {
+		$(".left li").css('backgroundColor','white')
+		$(this).css('backgroundColor','#eee');
+	});
+	$(".left li:nth-child(1)").click(function(){
+		$("iframe").attr('src', 'shangjia.html');
+	});
+	$(".left li:first-child").click();
+});
---
+++ a/js/shangjia.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+$("#ok").mouseover(function(){
+	$(this).css('backgroundColor', 'rgb(200,200,200)');
+});
+$("#ok").mouseleave(function(){
+	$(this).css('backgroundColor', '#eee');
+});
+});
